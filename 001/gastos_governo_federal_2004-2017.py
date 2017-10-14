b7aa6edbd2d7c31af7105c2a4cfd4067d71b1d34
lucas59356 <lucas59356@gmail.com>
2017-10-14 15:54:41 -0300

    Refatorando e fazendo uma limpeza no código

--- 001/gastos_governo_federal_2004-2017.py
+++ 001/gastos_governo_federal_2004-2017.py
@@ -11,12 +11,9 @@ import matplotlib.pyplot as plt
 import numpy as np
 from bs4 import BeautifulSoup
 
+reqUrl = "http://www.portaltransparencia.gov.br/PortalComprasDiretasOEOrgaoSubordinado.asp?Ano=%i&CodigoOS=%s"
 
-url1 = "http://www.portaltransparencia.gov.br/PortalComprasDiretasOEOrgaoSubordinado.asp?Ano="
-url2 = "&CodigoOS="
-
-year = list(range(2004,2018))
-dataArr = []
+year = range(2004,2018)
 bi = 1000000000.0
 
 #Presidência República: 20000
@@ -28,31 +25,30 @@ bi = 1000000000.0
 #Esporte: 51000
 
 
-def datas(k,c):
+def datas(k):
+    dataArr = []
     for i in year:
-        url = url1+str(i)+url2+str(k)
+        url = reqUrl %(i, k) # Usando sintaxe mais limpa para juntar os valores
         req = requests.get(url)
         bsObj = BeautifulSoup(req.content, "html.parser")
         data1 = bsObj.find_all("td", {"class":"colunaValor"}, limit=2)
-        data2 = (str(data1).replace("[<td class=\"colunaValor\">",""))
-        data2 = (str(data2).replace("<td class=\"colunaValor\">",""))
-        data2 = (str(data2).replace("</td>",""))
-        data2 = (str(data2).replace("]",""))
-        data2 = (str(data2).replace(".",""))
-        data2 = (str(data2).replace("\r\n",""))
-        data2 = (str(data2).replace(" ",""))
+        data2 = r2e(data1, "[<td class=\"colunaValor\">")  # Evitando repetição de código usand função externa
+        data2 = r2e(data2, "<td class=\"colunaValor\">")
+        data2 = r2e(data2, "</td>")
+        data2 = r2e(data2, "]")
+        data2 = r2e(data2, ".")
+        data2 = r2e(data2, "\r\n")
+        data2 = r2e(data2, " ")
         data2 = re.sub(r"^[0-9]*\,[0-9]*\,","", data2)
-        data2 = (str(data2).replace(",","."))
+        data2 = str(data2).replace(",",".")
 
-        dataArr.append(data2)
-
-        
-    dataArr = [float(j) for j in dataArr]
-    dataArr = np.array(dataArr)
-    dataArr = dataArr/c
+        dataArr.append(float(data2)) # Convertendo direto o dado pra não ser necessária reiteração
     print(dataArr)
-    return dataArr
+    return np.array(dataArr)/bi # Retornando direto
 
+# Replace de alguma coisa para valor vazio
+def r2e(data, pattern):
+    return str(data).replace(pattern, "")
 
 def plotCost(dataY, nameR):
     plt.grid(True, linestyle="--")
@@ -66,43 +62,40 @@ def plotCost(dataY, nameR):
    
     plt.show()
 
-
 def plotCostAll():
     plt.grid(True, linestyle="--")
     plt.title("Gastos Destinados pelo Governo Federal (2004-2017)\n", fontsize="20")
     plt.xlabel("Ano", fontsize="16")
     plt.ylabel("Em bilhões de R$", fontsize="16")
-    plt.plot(year, mPreR, label="Presidência da República")
-    plt.plot(year, mCien, label="Ciência e Tecnologia")
-    plt.plot(year, mEduc, label="Educação")
-    plt.plot(year, mPreS, label="Previdência Social")
-    plt.plot(year, mSaud, label="Saúde")
-    plt.plot(year, mMeiA, label="Meio Ambiente")
-    plt.plot(year, mEspo, label="Esporte")
+    plt.plot(year, v_presidencia, label="Presidência da República")
+    plt.plot(year, v_ciencia_tec, label="Ciência e Tecnologia")
+    plt.plot(year, v_educacao, label="Educação")
+    plt.plot(year, v_prev_social, label="Previdência Social")
+    plt.plot(year, v_saude, label="Saúde")
+    plt.plot(year, v_meio_ambiente, label="Meio Ambiente")
+    plt.plot(year, v_esporte, label="Esporte")
     
     legend = plt.legend(bbox_to_anchor=(1.01, 1),loc=2, fontsize=12, borderaxespad=0.)
     legend.get_frame().set_facecolor('#ffffff')
-    
+
     #plt.rcParams["figure.figsize"] = [15,7]
     #plt.savefig("educ-sau.png")
     plt.show()
 
+v_presidencia = datas(20000)
+v_ciencia_tec = datas(24000)
+v_educacao = datas(26000)
+v_prev_social = datas(33000)
+v_saude = datas(36000)
+v_meio_ambiente = datas(44000)
+v_esporte = datas(51000)
 
-mPreR = datas(20000, bi)
-mCien = datas(24000, bi)
-mEduc = datas(26000, bi)
-mPreS = datas(33000, bi)
-mSaud = datas(36000, bi)
-mMeiA = datas(44000, bi)
-mEspo = datas(51000, bi)
-
-
-plotCost(mPreR, "Presidência da República")
-plotCost(mCien, "Ciência e Tecnologia")
-plotCost(mEduc, "Educação")
-plotCost(mPreS, "Previdência Social")
-plotCost(mSaud, "Saúde")
-plotCost(mMeiA, "Meio Ambiente")
-plotCost(mEspo, "Esporte")
+plotCost(v_presidencia, "Presidência da República")
+plotCost(v_ciencia_tec, "Ciência e Tecnologia")
+plotCost(v_educacao, "Educação")
+plotCost(v_prev_social, "Previdência Social")
+plotCost(v_saude, "Saúde")
+plotCost(v_meio_ambiente, "Meio Ambiente")
+plotCost(v_esporte, "Esporte")
 
 plotCostAll()
