28cdd0fb9bd6adf795505a5b19c17667aeaa49ce
Edson Araújo <edsonlead@gmail.com>
2017-11-02 01:10:12 -0300

    limpeza

--- 001/main.py
+++ 001/main.py
@@ -1,22 +1,11 @@
-from gastos_governo import get_valores
-#from graph import grafico_individual, grafico_unificado
-from graph import grafico_individual
+from graph import *
 
-# Obtendo os valores
-v_presidencia = get_valores(20000)
-v_ciencia_tec = get_valores(24000)
-v_educacao = get_valores(26000)
-v_prev_social = get_valores(33000)
-v_saude = get_valores(36000)
-v_meio_ambiente = get_valores(44000)
-v_esporte = get_valores(51000)
+sinplot(v_presidencia, "Presidência da República")
+sinplot(v_ciencia_tec, "Ciência e Tecnologia")
+sinplot(v_educacao, "Educação")
+sinplot(v_prev_social, "Previdência Social")
+sinplot(v_saude, "Saúde")
+sinplot(v_meio_ambiente, "Meio Ambiente")
+sinplot(v_esporte, "Esporte")
 
-grafico_individual(v_presidencia, "Presidência da República")
-grafico_individual(v_ciencia_tec, "Ciência e Tecnologia")
-grafico_individual(v_educacao, "Educação")
-grafico_individual(v_prev_social, "Previdência Social")
-grafico_individual(v_saude, "Saúde")
-grafico_individual(v_meio_ambiente, "Meio Ambiente")
-grafico_individual(v_esporte, "Esporte")
-
-#grafico_unificado()
+multplot()
